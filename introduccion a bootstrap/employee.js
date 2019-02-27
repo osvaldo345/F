f53cdb708e4f7fdc768507a831a1c9834c163bd3
osvaldo345 <42743246+osvaldo345@users.noreply.github.com>
2019-02-26 19:34:58 -0600

    f

---
+++ introduccion a bootstrap/employee.js
@@ -0,0 +1,11 @@
+export default class employee {
+    constructor(name, email, birthday) {
+        this._name = name.toUpperCase();
+        this._email = email;
+        this._birthday = birthday;
+
+    }
+
+    //returns employee age
+    getAge() {}
+}
